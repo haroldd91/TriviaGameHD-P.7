0c2f89caaf0316906d40c89ad3c2ff6a50abf2cb
Diaz-Lunar <0216114@students.dadeschools.net>
2019-04-16 14:15:42 -0400

    finished html; started on js

---
+++ main.js
@@ -0,0 +1,23 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question set
+var questions = [
+    
+    {
+        question: "What animal weighs the most?",
+        answers: ["A elephant", "A giraffe", "A rhino", "A blue whale"],
+        correctAnswer: "A blue whale",
+        image: "assets\yea.gif"
+    },
+
+    {
+        question: "Which one of these animals are nocturnal?",
+        answers: ["Shark", "Dog", "Rabbit", "Deer"],
+        correctAnswer: "Shark",
+        image: "assets\yeaa.gif"
+    }
+
+
+]
